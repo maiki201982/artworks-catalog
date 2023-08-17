f32dd425ce7a89bdbc1eaea86801827b01f17085
Miguel Munoz <icmiguel.munoz@americanwell.com>
2023-08-17 16:15:31 -0300

    remove unnecesary import

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NotFoundComponent } from '../not-found/not-found/not-found.component';
 import { ArtworkListComponent } from './artwork-list/artwork-list.component';
 import { HomeComponent } from './home/home.component';
 import { RandomTableComponent } from './random-table/random-table.component';
